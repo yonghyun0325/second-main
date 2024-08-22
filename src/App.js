9b4c7730e5fd40c1c6e079d544bb550be564e325
yonghyun <dldydgus95426@gmail.com>
2024-08-22 12:45:43 +0900

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import { Link } from 'react-router-dom';
 function App() {
   const sliderSettings = {
     infinite: true,
-    speed: 500,
+    speed:500,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
